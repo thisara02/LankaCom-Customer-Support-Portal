fd8e008468ffaa722b9273ac86445aadc8f7e4d8
Thisara Madusanka <137640738+thisara02@users.noreply.github.com>
2025-06-03 12:03:51 +0530

    Land Page adjust

--- src/Pages/Customer/Landing.tsx
+++ src/Pages/Customer/Landing.tsx
@@ -1,27 +1,33 @@
 import React, { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import LandingVideo from "../../videos/land.mp4";
-// import Logo from "../assets/logo.png"; // Adjust if needed
+import LandingVideo from "../../videos/land2.mov";
 
 const Landing: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
+    const video = videoRef.current;
+
     const handleEnded = () => {
       navigate("/login");
     };
 
-    const video = videoRef.current;
     if (video) {
       video.addEventListener("ended", handleEnded);
-    }
 
-    return () => {
-      if (video) {
+      // Play only for 2 seconds
+      const timer = setTimeout(() => {
+        video.pause();
+        video.currentTime = 0; // reset to start if needed
+        navigate("/login");
+      }, 3000);
+      
+      return () => {
+        clearTimeout(timer);
         video.removeEventListener("ended", handleEnded);
-      }
-    };
+      };
+    }
   }, [navigate]);
 
   return (
@@ -40,16 +46,14 @@ const Landing: React.FC = () => {
 
       {/* Overlay content */}
       <div className="absolute inset-0 flex flex-col bg-black bg-opacity-0 text-center px-8">
-        {/* <div className="flex justify-right -mt-0 mb-0 pt-60 items-center justify-center">
-             <img src={Logo} alt="LankaCom" className="h-full" />
-        </div> */}
-
-
-        <h1 className="text-white text-6xl sm:text-6xl md:text-6xl font-bold font-jura animate-glow-text justify-center items-center text-center pt-80">
-           Cyber Security Support Portal
+        <h1 className="text-white text-9xl sm:text-6xl md:text-6xl font-bold font-jura animate-glow-text justify-center items-center text-center pt-80">
+          Welcome
         </h1>
-        <h1 className="text-white text-4xl sm:text-4xl md:text-4xl font-bold font-jura animate-glow-text justify-center items-center text-center pt-10">
-           Lanka Communication Services pvt Ltd
+        <h1 className="text-white text-9xl sm:text-6xl md:text-6xl font-bold font-jura animate-glow-text justify-center items-center text-center">
+          Cyber Security Support Portal
+        </h1>
+        <h1 className="text-white text-4xl sm:text-7xl md:text-4xl font-bold font-jura animate-glow-text justify-center items-center text-center pt-10">
+          Lanka Communication Services pvt Ltd
         </h1>
       </div>
     </div>
